6621f8bcc9348d8ebd674ace68612df77187113f
Mouzam ali <malikmouzam316@gmail.com>
2025-09-25 15:36:31 +0500

    updted user route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { protect } from "../middleware/auth.js";
 import jwt from "jsonwebtoken";
 
