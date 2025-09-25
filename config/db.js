73053b6d25c0b04f57a66fb6901154caac829f6b
Mouzam ali <malikmouzam316@gmail.com>
2025-09-25 15:22:38 +0500

    initial commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        // console.log("local hist connected successfully")
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+
+    }catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
