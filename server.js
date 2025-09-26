36879c8558bd025a11b256535cf5d58b10af2244
Mouzam ali <malikmouzam316@gmail.com>
2025-09-26 07:05:01 +0500

    cors addition

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import notesRoutes from "./routes/notes.js";
 import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
+app.use(cors({
+  origin: "https://my-notes-eight-eta.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.use(express.json());
 
@@ -19,7 +26,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/{*splat}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
